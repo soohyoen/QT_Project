9399a45bf08627b97486e526b39cd7bd2f62fcc1
soohyoen <gnejfejf789@naver.com>
2023-02-03 13:25:02 +0900

    Porting test

---
+++ Toy_project/initDb.h
@@ -0,0 +1,26 @@
+void addBook(QSqlQuery &q, const QString &title, int year, const QVariant &authorId,
+             const QVariant &genreId, int rating)
+{
+    q.addBindValue(title);
+    q.addBindValue(year);
+    q.addBindValue(authorId);
+    q.addBindValue(genreId);
+    q.addBindValue(rating);
+    q.exec();
+}
+
+QVariant addGenre(QSqlQuery &q, const QString &name)
+{
+    q.addBindValue(name);
+    q.exec();
+    return q.lastInsertId();
+}
+
+QVariant addAuthor(QSqlQuery &q, const QString &name, const QDate &birthdate)
+{
+    q.addBindValue(name);
+    q.addBindValue(birthdate);
+    q.exec();
+    return q.lastInsertId();
+}
+
